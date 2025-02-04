e73bbcfb00dbb1a9e9191dac675e04d77622d381
bytetang <6038077+bytetang@users.noreply.github.com>
2025-02-04 17:22:14 +0800

    Upgrade ecrecover hook (#509)
    * fix: upgrade the secp256k1 ecrecover hook
    
    * optimize code
    
    * fmt code

--- vm/src/emulator/riscv/hook/ecrecover.rs
+++ vm/src/emulator/riscv/hook/ecrecover.rs
@@ -1,30 +1,56 @@
 use super::super::riscv_emulator::RiscvEmulator;
-use crate::chips::gadgets::curves::k256::{Invert, RecoveryId, Signature, VerifyingKey};
+use k256::{elliptic_curve::ff::PrimeField, FieldBytes, FieldElement, Scalar as K256Scalar};
+
+/// The non-quadratic residue for the curve for secp256k1.
+const NQR: [u8; 32] = {
+    let mut nqr = [0; 32];
+    nqr[31] = 3;
+    nqr
+};
 
 pub fn ecrecover(_: &RiscvEmulator, buf: &[u8]) -> Vec<Vec<u8>> {
-    assert_eq!(
-        buf.len(),
-        65 + 32,
-        "ecrecover input should have length 65 + 32"
-    );
-    let (sig, msg_hash) = buf.split_at(65);
-    let sig: &[u8; 65] = sig.try_into().unwrap();
-    let msg_hash: &[u8; 32] = msg_hash.try_into().unwrap();
+    // Early return if the buffer length is incorrect
+    if buf.len() != 65 {
+        return vec![vec![0]];
+    }
 
-    let mut recovery_id = sig[64];
-    let mut sig = Signature::from_slice(&sig[..64]).unwrap();
+    let r_is_y_odd = buf[0] & 0b1000_0000 != 0;
 
-    if let Some(sig_normalized) = sig.normalize_s() {
-        sig = sig_normalized;
-        recovery_id ^= 1;
-    };
-    let recid = RecoveryId::from_byte(recovery_id).expect("Computed recovery ID is invalid!");
+    // Directly convert slices to arrays without intermediate steps
+    let r_bytes: [u8; 32] = buf[1..33].try_into().unwrap();
+    let alpha_bytes: [u8; 32] = buf[33..65].try_into().unwrap();
 
-    let recovered_key = VerifyingKey::recover_from_prehash(&msg_hash[..], &sig, recid).unwrap();
-    let bytes = recovered_key.to_sec1_bytes();
+    // Convert bytes to field elements
+    let r = FieldElement::from_bytes(&FieldBytes::from(r_bytes)).unwrap();
+    let alpha = FieldElement::from_bytes(&FieldBytes::from(alpha_bytes)).unwrap();
 
-    let (_, s) = sig.split_scalars();
-    let s_inverse = s.invert();
+    // Early return if r or alpha is zero
+    if bool::from(r.is_zero()) || bool::from(alpha.is_zero()) {
+        return vec![vec![0]];
+    }
 
-    vec![bytes.to_vec(), s_inverse.to_bytes().to_vec()]
+    // Normalize the y-coordinate always to be consistent.
+    if let Some(mut y_coord) = alpha.sqrt().into_option().map(|y| y.normalize()) {
+        let r = K256Scalar::from_repr(r.to_bytes()).unwrap();
+        let r_inv = r.invert().expect("Non zero r scalar");
+
+        if r_is_y_odd != bool::from(y_coord.is_odd()) {
+            y_coord = y_coord.negate(1);
+            y_coord = y_coord.normalize();
+        }
+
+        vec![
+            vec![1],
+            y_coord.to_bytes().to_vec(),
+            r_inv.to_bytes().to_vec(),
+        ]
+    } else {
+        let nqr_field = FieldElement::from_bytes(FieldBytes::from_slice(&NQR)).unwrap();
+        let qr = alpha * nqr_field;
+        let root = qr
+            .sqrt()
+            .expect("if alpha is not a square, then qr should be a square");
+
+        vec![vec![0], root.to_bytes().to_vec()]
+    }
 }
