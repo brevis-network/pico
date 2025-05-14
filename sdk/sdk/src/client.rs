4559ca0cfcde9210b7644a67ff6f816e0a96b793
eason <eason@brevis.network>
2025-05-14 20:47:22 +0800

    add `verify` to prover client

--- sdk/sdk/src/client.rs
+++ sdk/sdk/src/client.rs
@@ -133,6 +133,21 @@ macro_rules! create_sdk_prove_client {
                 Ok((riscv_proof, proof))
             }
 
+            /// verify the riscv and embed proof
+            pub fn verify(
+                &self,
+                proof: &(MetaProof<$sc>, MetaProof<$bn254_sc>),
+            ) -> Result<(), Error> {
+                let riscv_vk = self.riscv_vk();
+                if !self.riscv.verify(&proof.0, riscv_vk) {
+                    return Err(Error::msg("verify riscv proof failed"));
+                }
+                if !self.embed.verify(&proof.1, riscv_vk) {
+                    return Err(Error::msg("verify embed proof failed"));
+                }
+                Ok(())
+            }
+
             pub fn write_onchain_data(
                 &self,
                 outdir: impl AsRef<Path>,
