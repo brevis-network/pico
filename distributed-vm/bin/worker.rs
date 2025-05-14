46776668b6e5668b5405f45386eaec5876e54e75
eason <eason@brevis.network>
2025-05-14 21:29:41 +0800

    update coordinator-worker for gpu

--- distributed-vm/bin/worker.rs
+++ distributed-vm/bin/worker.rs
@@ -10,6 +10,13 @@ use log::debug;
 use pico_perf::common::bench_field::BenchField;
 use pico_vm::{
     configs::stark_config::{BabyBearPoseidon2, KoalaBearPoseidon2},
+    cuda_adaptor::{
+        chips_analyzer::initial_chips_load,
+        resource_pool::{
+            mem_pool::{create_ctx, get_global_mem_pool},
+            stream_pool::{create_stream, get_global_stream_pool},
+        },
+    },
     machine::logger::setup_logger,
     thread::channel::DuplexUnboundedChannel,
 };
@@ -31,6 +38,8 @@ async fn main() -> Result<()> {
 
     let grpc_conn = grpc::new_channel(cfg.coordinator_grpc_addr.clone()).await;
 
+    initial_chips_load();
+
     match cfg.field {
         BenchField::BabyBear => {
             (0..cfg.prover_count).enumerate().for_each(|(i, _)| {
@@ -46,11 +55,28 @@ async fn main() -> Result<()> {
                     shutdown.clone(),
                 );
 
+                create_ctx(i);
+                let sync_pool = get_global_mem_pool(i);
+                let mem_pool = &sync_pool.0;
+
+                create_stream(i);
+                let sync_stream = get_global_stream_pool(i);
+                let stream = &sync_stream.0;
+
                 // start prover
-                let prover_id = format!("{}-prover-{}", cfg.worker_name, i);
-                let prover =
-                    Prover::<BabyBearPoseidon2>::new(prover_id, cfg.program, channel.endpoint2());
-                prover.run();
+                let prover_id = format!("prover-{}", i);
+                // let prover =
+                //     Prover::<BabyBearPoseidon2>::new(prover_id, cfg.program, channel.endpoint2());
+                // prover.run();
+                let (prover, pk_gm) = Prover::<BabyBearPoseidon2>::new_cuda(
+                    prover_id,
+                    cfg.program,
+                    channel.endpoint2(),
+                    stream,
+                    mem_pool,
+                    i,
+                );
+                prover.run_cuda(pk_gm, stream, mem_pool, i);
             });
         }
         BenchField::KoalaBear => {
@@ -67,11 +93,28 @@ async fn main() -> Result<()> {
                     shutdown.clone(),
                 );
 
+                create_ctx(i);
+                let sync_pool = get_global_mem_pool(i);
+                let mem_pool = &sync_pool.0;
+
+                create_stream(i);
+                let sync_stream = get_global_stream_pool(i);
+                let stream = &sync_stream.0;
+
                 // start prover
-                let prover_id = format!("{}-prover-{}", cfg.worker_name, i);
-                let prover =
-                    Prover::<KoalaBearPoseidon2>::new(prover_id, cfg.program, channel.endpoint2());
-                prover.run();
+                let prover_id = format!("prover-{}", i);
+                // let prover =
+                //     Prover::<KoalaBearPoseidon2>::new(prover_id, cfg.program, channel.endpoint2());
+                // prover.run();
+                let (prover, pk_gm) = Prover::<KoalaBearPoseidon2>::new_cuda(
+                    prover_id,
+                    cfg.program,
+                    channel.endpoint2(),
+                    stream,
+                    mem_pool,
+                    i,
+                );
+                prover.run_cuda(pk_gm, stream, mem_pool, i);
             });
         }
     };
