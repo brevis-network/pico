e3cb3028f8f00b557b73531c22cdad8bf99c22c3
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-06-04 16:58:20 +0300

    Update mod.rs

--- vm/src/chips/chips/alu/add_sub/mod.rs
+++ vm/src/chips/chips/alu/add_sub/mod.rs
@@ -6,7 +6,7 @@ pub mod traces;
 
 /// A chip that implements addition for the opcode ADD and SUB.
 ///
-/// SUB is basically an ADD with a re-arrangment of the operands and result.
+/// SUB is basically an ADD with a re-arrangement of the operands and result.
 /// E.g. given the standard ALU op variable name and positioning of `a` = `b` OP `c`,
 /// `a` = `b` + `c` should be verified for ADD, and `b` = `a` + `c` (e.g. `a` = `b` - `c`)
 /// should be verified for SUB.
