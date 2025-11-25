a566a4d0d69124e14d2e2f032dab12127330b630
kaiwei-0 <kaiwei@brevis.network>
2025-11-25 22:18:52 +0800

    soundcalc: comment out redundant proverchain code

--- vm/examples/test_proverchain.rs
+++ vm/examples/test_proverchain.rs
@@ -54,39 +54,39 @@ fn main() {
             bytes.len() as f64 / 1024.0,
         );
     }
-    info!("Proving RECURSION..");
-    let proof = convert.prove(proof);
-    assert!(convert.verify(&proof, riscv_vk));
-    let proof = combine.prove(proof);
-    assert!(combine.verify(&proof, riscv_vk));
-    let proof = compress.prove(proof);
-    assert!(compress.verify(&proof, riscv_vk));
-    let proof = embed.prove(proof);
-    assert!(embed.verify(&proof, riscv_vk));
-
-    info!("ProverChain on KoalaBear succeeded.");
-
-    log_section("BB PROVER CHAIN");
-    let riscv = RiscvProver::new_initial_prover((RiscvBBSC::new(), elf), Default::default(), None);
-    let convert = ConvertProver::new_with_prev(&riscv, Default::default(), None);
-    let combine = CombineProver::new_with_prev(&convert, Default::default(), None);
-    let compress = CompressProver::new_with_prev(&combine, Default::default(), None);
-    let embed = EmbedProver::<_, _, Vec<u8>>::new_with_prev(&compress, Default::default(), None);
-
-    let riscv_vk = riscv.vk();
-
-    info!("Proving RISCV..");
-    let proof = riscv.prove(riscv_stdin);
-    assert!(riscv.verify(&proof, riscv_vk));
-    info!("Proving RECURSION..");
-    let proof = convert.prove(proof);
-    assert!(convert.verify(&proof, riscv_vk));
-    let proof = combine.prove(proof);
-    assert!(combine.verify(&proof, riscv_vk));
-    let proof = compress.prove(proof);
-    assert!(compress.verify(&proof, riscv_vk));
-    let proof = embed.prove(proof);
-    assert!(embed.verify(&proof, riscv_vk));
-
-    info!("ProverChain on BabyBear succeeded.");
+    // info!("Proving RECURSION..");
+    // let proof = convert.prove(proof);
+    // assert!(convert.verify(&proof, riscv_vk));
+    // let proof = combine.prove(proof);
+    // assert!(combine.verify(&proof, riscv_vk));
+    // let proof = compress.prove(proof);
+    // assert!(compress.verify(&proof, riscv_vk));
+    // let proof = embed.prove(proof);
+    // assert!(embed.verify(&proof, riscv_vk));
+    //
+    // info!("ProverChain on KoalaBear succeeded.");
+    //
+    // log_section("BB PROVER CHAIN");
+    // let riscv = RiscvProver::new_initial_prover((RiscvBBSC::new(), elf), Default::default(), None);
+    // let convert = ConvertProver::new_with_prev(&riscv, Default::default(), None);
+    // let combine = CombineProver::new_with_prev(&convert, Default::default(), None);
+    // let compress = CompressProver::new_with_prev(&combine, Default::default(), None);
+    // let embed = EmbedProver::<_, _, Vec<u8>>::new_with_prev(&compress, Default::default(), None);
+    //
+    // let riscv_vk = riscv.vk();
+    //
+    // info!("Proving RISCV..");
+    // let proof = riscv.prove(riscv_stdin);
+    // assert!(riscv.verify(&proof, riscv_vk));
+    // info!("Proving RECURSION..");
+    // let proof = convert.prove(proof);
+    // assert!(convert.verify(&proof, riscv_vk));
+    // let proof = combine.prove(proof);
+    // assert!(combine.verify(&proof, riscv_vk));
+    // let proof = compress.prove(proof);
+    // assert!(compress.verify(&proof, riscv_vk));
+    // let proof = embed.prove(proof);
+    // assert!(embed.verify(&proof, riscv_vk));
+    //
+    // info!("ProverChain on BabyBear succeeded.");
 }
